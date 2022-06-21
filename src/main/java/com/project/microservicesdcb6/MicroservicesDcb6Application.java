ea7eb716e2c8b3d9aa77b6934b808dff1d07c8d5
Emerson Millard <emillard.code@gmail.com>
2022-06-21 12:00:00 -0700

    Added annotations to main class.

--- src/main/java/com/project/microservicesdcb6/MicroservicesDcb6Application.java
+++ src/main/java/com/project/microservicesdcb6/MicroservicesDcb6Application.java
@@ -2,12 +2,18 @@ package com.project.microservicesdcb6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class MicroservicesDcb6Application {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(MicroservicesDcb6Application.class, args);
+
     }
 
 }
